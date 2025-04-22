2b61f4726fc98c20ec22aac5c6a9c12fd0246598
einartech <imalovelyghost13@gmail.com>
2025-04-22 22:34:36 +0200

    uploat data

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 ### Description
 
-A simple portfolio template for developer/designers built with React. 
-
-### [live preview](https://ubaimutl.github.io/react-portfolio/)
-
-[![react portfoiio](src/assets/images/react%20portfolio%20gif.gif)](https://ubaimutl.github.io/react-portfolio/)
+A simple portfolio template for developer/designers built with React.
 
 ### Features
 
@@ -18,12 +14,11 @@ A simple portfolio template for developer/designers built with React.
 
 Get the code
 
-<pre>git clone https://github.com/ubaimutl/react-portfolio.git</pre>
- 
+<pre>git clone https://github.com/einartech/react-portfolio.git</pre>
+
 Install required dependencies
 
 <pre>yarn install</pre>
-
 
 Start the server
 
@@ -31,8 +26,8 @@ Start the server
 
 ### More
 
-Modify pages content in  `src/content_option.js`.
+Modify pages content in `src/content_option.js`.
 
 ### Thanks
 
-If you like this portfolio template don't forget give it a ⭐ 
+If you like this portfolio template don't forget give it a ⭐
--- src/content_option.js
+++ src/content_option.js
@@ -2,82 +2,141 @@ const logotext = "EINARTECH";
 const meta = {
   title: "Einar Sanchez",
   description:
-    "I’m Einar Sanchez Security Software Development & Ciso Advisory,currently working in Barcelona",
+    "I’m Einar Sanchez, Security Software Development & CISO Advisory, currently working in Barcelona.",
 };
 
 const introdata = {
   title: "I’m Einar Sanchez",
   animated: {
     first: "I love coding",
-    second: "I burp some websites :p",
-    third: "I love discovering new things",
+    second: "I specialize in cybersecurity",
+    third: "I optimize software and architecture",
   },
   description:
-    "Any sufficiently advanced technology is indistinguishable from magic",
+    "Any sufficiently advanced technology is indistinguishable from magic.",
   your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
   title: "A bit about myself",
   aboutme:
-    "I have been passionate about technology since childhood, and my curiosity has driven me to learn and understand various aspects of the tech world through self-study. Over the years, I have gained work experience and training in different areas related to technology, allowing me to develop a solid and versatile knowledge base. Currently, my focus is on cybersecurity. I am particularly interested in the Bug Bounty world, and my goal is to specialize in creating secure and ethical applications. My passion for improving digital security motivates me to continue learning and contributing to a safer online environment for everyone. My objective is to keep developing my technical skills, collaborate with other professionals, and add value to projects that promote security in technology in an ethical and responsible way.",
+    "I have been passionate about technology since childhood, and my curiosity has driven me to learn and understand various aspects of the tech world through self-study. Over the years, I have gained work experience and training in different areas related to technology, allowing me to develop a solid and versatile knowledge base. Currently, my focus is on cybersecurity. My objective is to keep developing my technical skills, collaborate with other professionals, and add value to projects that promote security in technology in an ethical and responsible way.",
 };
+
 const worktimeline = [
   {
-    jobtitle: "Dev of week",
-    where: "YAdfi",
-    date: "2020",
+    jobtitle: "Security Software Development & CISO Advisory",
+    where: "Ajuntament de Terrassa",
+    date: "February 2023 - Present",
+    description:
+      "Provided technical guidance on Software Development and Architecture Optimization for a Job Position Management System.",
   },
   {
-    jobtitle: "Dev of week",
-    where: "Jamalya",
-    date: "2019",
+    jobtitle: "Software Development & Architecture Optimization",
+    where: "Quantion",
+    date: "April 2022 - January 2023",
+    description:
+      "Updated and maintained a back office and approximately 25 web pages, focusing on a monolithic project requiring constant care and detail.",
   },
   {
-    jobtitle: "Dev of week",
-    where: "ALquds",
-    date: "2019",
+    jobtitle: "API Software Development",
+    where: "Tappx",
+    date: "November 2021 - April 2022",
+    description:
+      "Created, maintained, and updated tools to improve the company's service, focusing on customer integrations.",
+  },
+  {
+    jobtitle: "Software Development & Hardware Optimization",
+    where: "Admira Digital Networks",
+    date: "August 2018 - August 2021",
+    description:
+      "Conducted software reviews to enhance performance, identified failures, and proposed hardware improvements.",
   },
 ];
 
 const skills = [
-  {
-    name: "Python",
-    value: 90,
-  },
-  {
-    name: "Djano",
-    value: 85,
-  },
-  {
-    name: "Javascript",
-    value: 80,
-  },
-  {
-    name: "React",
-    value: 60,
-  },
-  {
-    name: "Jquery",
-    value: 85,
-  },
+  // Frontend
+  { name: "HTML", value: 90 },
+  { name: "CSS", value: 85 },
+  { name: "Sass", value: 80 },
+  { name: "CSS Modules", value: 75 },
+  { name: "JavaScript", value: 90 },
+  { name: "AngularJS", value: 70 },
+  { name: "Bootstrap", value: 80 },
+  { name: "jQuery", value: 85 },
+  { name: "React", value: 80 },
+
+  // Backend
+  { name: "Node.js", value: 85 },
+  { name: "PHP", value: 70 },
+  { name: "Java", value: 75 },
+  { name: "Express.js", value: 80 },
+
+  // Databases
+  { name: "MySQL", value: 85 },
+  { name: "MongoDB", value: 80 },
+
+  // Containers and DevOps
+  { name: "Docker", value: 75 },
+  { name: "Jenkins", value: 70 },
+
+  // Security and Networking
+  { name: "Zenmap", value: 70 },
+  { name: "Putty", value: 75 },
+  { name: "Burp Suite", value: 80 },
+  { name: "Fuzz", value: 70 },
+  { name: "VPN", value: 85 },
+  { name: "SSH", value: 85 },
+
+  // Device Development
+  { name: "Rooting Android", value: 70 },
+  { name: "ODIN", value: 65 },
+  { name: "Device Rooting", value: 70 },
+  { name: "Termite", value: 60 },
+  { name: "KNX", value: 65 },
+
+  // Others
+  { name: "JSON", value: 85 },
+  { name: "WordPress", value: 80 },
+  { name: "Notion", value: 75 },
+
+  // Development and Testing Tools
+  { name: "Postman", value: 85 },
+  { name: "JUnit Jupiter", value: 75 },
+  { name: "Vitest", value: 70 },
+  { name: "Vite", value: 70 },
+
+  // Operating Systems
+  { name: "Windows", value: 90 },
+  { name: "Linux", value: 85 },
+  { name: "Kali Linux", value: 80 },
+
+  // Editors and Shells
+  { name: "Bash", value: 75 },
+  { name: "Vim", value: 70 },
+  { name: "PowerShell", value: 75 },
+  { name: "VSCode", value: 90 },
+
+  // Version Control and Collaboration
+  { name: "Git", value: 85 },
+  { name: "GitHub", value: 85 },
 ];
 
 const services = [
   {
-    title: "UI & UX Design",
+    title: "Security Software Development",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+      "Specialized in creating secure and ethical applications to improve digital security.",
   },
   {
-    title: "Mobile Apps",
+    title: "Architecture Optimization",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+      "Design and optimize software architecture to enhance scalability and performance.",
   },
   {
-    title: "Wordpress Design",
+    title: "API Development",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+      "Develop and maintain APIs to improve integrations and customer services.",
   },
 ];
 
@@ -158,22 +217,20 @@ const dataportfolio = [
 ];
 
 const contactConfig = {
-  YOUR_EMAIL: "contact@einartech.com",
-  YOUR_FONE: "(34)618443166",
-  description: "Miau miau miau :)  ",
-  // creat an emailjs.com account
-  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_EMAIL: "grelsare13@gmail.com",
+  YOUR_FONE: "(34) 618443166",
+  description:
+    "Feel free to reach out to me for collaborations or inquiries about my work.",
   YOUR_SERVICE_ID: "service_id",
   YOUR_TEMPLATE_ID: "template_id",
   YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-  github: "https://github.com",
-  facebook: "https://facebook.com",
-  linkedin: "https://linkedin.com",
-  twitter: "https://twitter.com",
+  github: "https://github.com/einartech",
+  linkedin: "https://www.linkedin.com/in/einartech/",
 };
+
 export {
   meta,
   dataabout,
